1d3b7c5ebd6f8bf4e7d1583f54b45567e993b8be
macbook <iv-litovchenko@mail.ru>
2026-03-31 22:18:26 +0300

    PHPAirButton

--- public/airlogin.php
+++ public/airlogin.php
@@ -2,6 +2,10 @@
 
 use IvLitovchenko\PhpAirButtons\PhpAirButtons;
 
+error_reporting(E_ALL);
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', '1');
+
 require_once __DIR__ . '/../src/PhpAirButtons.php';
 
 $error    = null;
